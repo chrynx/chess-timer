1202162ad401f91adc47ab52ad0e5cf7586afa6d
chrynx <ralphmadriaga@gmail.com>
2017-11-24 14:09:05 +0000

    sorting a bug out. The timer still works a minute after the time runs out

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ $(()=>{
   const $secondsP2 = $('.secondsP2');
   const $millisecondsP1 = $('.millisecondsP1');
   const $millisecondsP2 = $('.millisecondsP2');
+  const $playerOnePlus = $('.playerOnePlus');
+  const $playerTwoPlus = $('.playerTwoPlus');
+  const $playerOneMinus = $('.playerOneMinus');
+  const $playerTwoMinus = $('.playerTwoMinus');
+  const $resetButton = $('.resetButton');
   //----------------------------------
   // ==========================================
   //--------------normal variables--------------
@@ -21,14 +26,38 @@ $(()=>{
   //--------------------------------------------
   //=============================================
   //-------------functions-------------
-  const timer = () => {
+
+  const timer = setInterval(() => {
+    if(minutesP1 < 0 && secondsP1 === 0 && millisecondsP1 === 0){
+      playerTurn = 'end';
+      $('.breaker').text('');
+      $minutesP1.text('');
+      $minutesP2.text('');
+      $secondsP2.text('WIN');
+      $secondsP1.text('LOSE');
+      $millisecondsP1.text('');
+      $millisecondsP2.text('');
+      clearInterval(timer);
+    }
+    if(minutesP2 < 0 && secondsP2 === 0 && millisecondsP2 === 0){
+      playerTurn = 'end';
+      $('.breaker').text('');
+      $minutesP1.text('');
+      $minutesP2.text('');
+      $secondsP2.text('LOSE');
+      $secondsP1.text('WIN');
+      $millisecondsP1.text('');
+      $millisecondsP2.text('');
+      clearInterval(timer);
+    }
     if(playerTurn === 'p1'){
       millisecondsP1 = millisecondsP1 - 1;
       if(millisecondsP1 >= 0) $millisecondsP1.text(millisecondsP1);
+      if(minutesP1 === 0 && secondsP1 === 0 && millisecondsP1 === 0);
       if(millisecondsP1 < 0){
-        console.log('got inside milli if');
         millisecondsP1 = 59;
         $millisecondsP1.text(millisecondsP1);
+        if(minutesP1 === 0 && secondsP1 === 0) $secondsP1.text(0);
         secondsP1 = secondsP1 - 1;
         if(secondsP1 >= 0) $secondsP1.text(secondsP1);
         if(secondsP1 < 0){
@@ -36,37 +65,32 @@ $(()=>{
           $secondsP1.text(secondsP1);
           minutesP1 = minutesP1 - 1;
           if(minutesP1 > 0) $minutesP1.text(minutesP1);
-          if(minutesP1 === 0){
-            $minutesP1.text('LOSE');
-            $secondsP1.text('LOSE');
-            $millisecondsP1.text('LOSE');
-          }
+          if(minutesP1 === 0) $minutesP1.text(0);
         }
       }
     }
     if(playerTurn === 'p2'){
       millisecondsP2 = millisecondsP2 - 1;
       if(millisecondsP2 >= 0) $millisecondsP2.text(millisecondsP2);
+      if(minutesP2 === 0 && secondsP2 === 0 && millisecondsP2 === 0) $millisecondsP2.text(0);
       if(millisecondsP2 < 0){
-        console.log('got inside milli if');
         millisecondsP2 = 59;
         $millisecondsP2.text(millisecondsP2);
         secondsP2 = secondsP2 - 1;
         if(secondsP2 >= 0) $secondsP2.text(secondsP2);
+        if(minutesP2 === 0 && secondsP2 === 0) $secondsP2.text(0);
         if(secondsP2 < 0){
           secondsP2 = 59;
           $secondsP2.text(secondsP2);
           minutesP2 = minutesP2 - 1;
           if(minutesP2 > 0) $minutesP2.text(minutesP2);
-          if(minutesP2 === 0){
-            $minutesP2.text('LOSE');
-            $secondsP2.text('LOSE');
-            $millisecondsP2.text('LOSE');
-          }
+          if(minutesP2 === 0) $minutesP2.text(0);
         }
       }
     }
-  };
+  }, 1/60);
+
+
   // ----------------------------------
   // =========================================
   //----------------events--------------
@@ -79,6 +103,38 @@ $(()=>{
     timer();
   });
 
+  $playerOnePlus.on('click', () => {
+    $minutesP1.text(minutesP1 + 1);
+    minutesP1 = minutesP1 + 1;
+  });
+  $playerTwoPlus.on('click', () => {
+    $minutesP2.text(minutesP2 + 1);
+    minutesP2 += 1;
+  });
+  $playerOneMinus.on('click', () => {
+    $minutesP1.text(minutesP1 - 1);
+    minutesP1 -= 1;
+  });
+  $playerTwoMinus.on('click', () => {
+    $minutesP2.text(minutesP2 - 1);
+    minutesP2 -= 1;
+  });
+  $resetButton.on('click', () => {
+    minutesP1 = 20;
+    minutesP2 = 20;
+    secondsP1 = 0;
+    secondsP2 = 0;
+    millisecondsP1 = 0;
+    millisecondsP2 = 0;
+    $('.breaker').text(':');
+    $minutesP1.text(minutesP1);
+    $secondsP1.text(secondsP1);
+    $millisecondsP1.text(millisecondsP1);
+    $minutesP2.text(minutesP2);
+    $secondsP2.text(secondsP2);
+    $millisecondsP2.text(millisecondsP2);
+    playerTurn = 'begin';
+  });
   // =x=x=x=x=x=x=x=x=x=x=
   // =x END OF THE FILE x=
   // =x=x+x=x=x=x=x=x=x=x=
